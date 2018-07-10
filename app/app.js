7af66d4bd76cd910c079d3c1585d33198bed3b22
vitaliidanchul <danchul.vitalii@gmail.com>
2018-07-10 19:04:54 +0300

    app logic with slots

--- app/app.js
+++ app/app.js
@@ -13,71 +13,69 @@ const config = {
 };
 
 const app = new App(config);
-
-let path = './survey_data/survey.json';
-let content = function () {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf-8', (err, data) => {
-            if (err) reject(err);
-            else resolve(data);
-        })
-    })
-}
-
+const survey = require('../survey_data/survey.json');
 
 // =================================================================================
 // App Logic
 // =================================================================================
-
 app.setHandler({
     'LAUNCH': function () {
-        content().then(survey => {
-            this.user().data.surveyObject = JSON.parse(survey);
-        });
-
         this.toIntent('GetFirstSurveyIntent');
     },
     'GetFirstSurveyIntent': function () {
-        if (this.user().data.surveyObject !== undefined) {
+        if (survey !== undefined) {
+            let inputs = this.getInputs();
+            // let { mainQuestionIndex, subQuestionIndex } = this.user().data;
+            console.log("slots: ", inputs);
             let speech, mainQuestion, subQuestion;
 
             if (this.user().data.mainQuestionIndex === undefined && this.user().data.subQuestionIndex === undefined) {
                 this.user().data.mainQuestionIndex = 0;
                 this.user().data.subQuestionIndex = 0;
+                this.user().data.prevIntent;
             }
-            if (this.user().data.mainQuestionIndex === this.user().data.surveyObject.surveyQuestions.length) {
+            if (this.user().data.mainQuestionIndex === survey.surveyQuestions.length) {
                 console.log("survey finished");
                 this.tell("survey finished");
-
             } else {
-                for (let i = this.user().data.mainQuestionIndex; i < this.user().data.surveyObject.surveyQuestions.length; i++) {
+                for (let i = this.user().data.mainQuestionIndex; i < survey.surveyQuestions.length; i++) {
 
-                    if (this.user().data.surveyObject.surveyQuestions[i].hasOwnProperty('surveyQuestions')) {
+                    if (survey.surveyQuestions[i].hasOwnProperty('surveyQuestions')) {
 
-                        mainQuestion = this.user().data.surveyObject.surveyQuestions[i].question;
+                        mainQuestion = survey.surveyQuestions[i].question;
 
-                        for (let j = this.user().data.subQuestionIndex; j < this.user().data.surveyObject.surveyQuestions[i].surveyQuestions.length; j++) {
 
-                            subQuestion = this.user().data.surveyObject.surveyQuestions[i].surveyQuestions[j].question;
+                        if (this.user().data.subQuestionIndex === survey.surveyQuestions[i].surveyQuestions.length) {
+                            console.log("go to main question")
+                            this.user().data.subQuestionsFinished = true;
+                            this.user().data.mainQuestionIndex++;
+                            this.user().data.subQuestionIndex = 0;
 
-                            this.tell(subQuestion);
+                        }
+                        for (let j = this.user().data.subQuestionIndex; j < survey.surveyQuestions[i].surveyQuestions.length; j++) {
 
-                            this.user().data.subQuestionIndex++;
+                            subQuestion = survey.surveyQuestions[i].surveyQuestions[j].question;
 
-                            if (this.user().data.subQuestionIndex === this.user().data.surveyObject.surveyQuestions[i].surveyQuestions.length) {
-                                this.user().data.subQuestionsFinished = true;
-                                this.user().data.mainQuestionIndex++;
-                                this.user().data.subQuestionIndex = 0;
-                                console.log("go to next question");
+                            this.user().data.subQuestionsFinished = false;
+
+
+                            if (this.user().data.prevIntent && !this.user().data.subQuestionsFinished) {
+                                this.ask(subQuestion);
+                                this.user().data.subQuestionIndex++;
+                                // this.user().data.prevIntent = false;
+                            } else {
+                                console.log("mainQuestion: ")
+                                this.ask(mainQuestion);
                             }
+
                             break;
                         }
                         break;
                     }
                     else {
 
-                        mainQuestion = this.user().data.surveyObject.surveyQuestions[i].question;
-                        this.tell(`${mainQuestion}`);
+                        mainQuestion = survey.surveyQuestions[i].question;
+                        this.ask(`${mainQuestion}`);
                         this.user().data.mainQuestionIndex++;
 
                         break;
@@ -86,6 +84,14 @@ app.setHandler({
                 }
             }
         }
+    },
+    'ToAnswersIntent': function () {
+        // console.log("this: ", this.requestObj.request.intent.name);
+        this.user().data.prevIntent = true;
+        this.toIntent('GetFirstSurveyIntent');
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.ask("Repeat");
     }
 });
 
