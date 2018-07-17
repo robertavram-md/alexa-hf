36d1ff08606d0608c4ead5d0304ac7dafd08ee5e
danchul_vitalii <danchul_vitalii@bitbucket.org>
2018-07-17 16:41:15 +0300

    subq slot validation

--- app/app.js
+++ app/app.js
@@ -85,12 +85,36 @@ app.setHandler({
                                     if (prevMainQuestion !== undefined && prevSubQuestion !== undefined && inputs !== undefined) {
                                         console.log("SUBQUESTIONS VALIDATION");
                                         let answerValue = compareUserInputAndAnswer.call(this, prevMainQuestion, prevSubQuestion, inputs);
+                                        if(answerValue !== undefined) {
+                                            console.log("SUBQ_ANSWER_VAUE: ", answerValue);
 
+                                            this.ask(subQuestion);
+
+                                            this.user().data.questionToRepeat = subQuestion;
+                                            
+                                            this.user().data.subQuestionIndex++;
+                                            this.user().data.correctSubAnswer = true;
+                                        } else {
+                                            let valuesToRead = getQuestionAnswers.call(this, prevMainQuestion, prevSubQuestion);
+                                            if(valuesToRead !== undefined ) {
+                                                console.log("VALUES TO READ :", valuesToRead);
+                                                this.ask(`Sorry, this answer is not valid. You can only choose from: ${valuesToRead}. What's your choice? `);
+                                                this.user().data.correctSubAnswer = false;
+                                            } 
+                                        }
+
+                                    } else {
+                                        console.log("NOT DEFINED SUBQUESTION");
+                                        if (this.user().data.simpleQueston !== true) {
+                                            this.user().data.mainQuestionIndex++;
+                                        }
+
+                                        this.user().data.questionToRepeat = subQuestion;
+                                        this.ask(subQuestion);
                                     }
 
                                 })
-                                this.ask(subQuestion);
-                                this.user().data.subQuestionIndex++;
+                               
 
                             } else if (this.user().data.prevIntent === false) {
                                 console.log("if null");
@@ -109,7 +133,10 @@ app.setHandler({
                                             console.log("SAVE ANSWER TO DB");
                                             console.log("answerValue :", answerValue);
                                             // this.user().data.mainQuestionIndex++;
-                                            this.ask(`${mainQuestion}`);
+                                            this.ask(mainQuestion);
+
+                                            this.user().data.questionToRepeat = mainQuestion;
+
                                             this.user().data.correctMainAnswer = true;
                                             delete this.user().data.subQuestionsFinished;
                                         } else {
@@ -126,7 +153,9 @@ app.setHandler({
                                         if (this.user().data.hardQuestion !== true) {
                                             this.user().data.mainQuestionIndex++;
                                         }
-                                        this.ask(`${mainQuestion}`);
+                                        this.ask(mainQuestion);
+
+                                        this.user().data.questionToRepeat = mainQuestion;
                                     }
 
                                 });
@@ -147,7 +176,10 @@ app.setHandler({
                                             console.log("SAVE ANSWER TO DB");
                                             console.log("answerValue :", answerValue);
                                             // this.user().data.mainQuestionIndex++;
-                                            this.ask(`${mainQuestion}`);
+                                            this.ask(mainQuestion);
+
+                                            this.user().data.questionToRepeat = mainQuestion;
+
                                             this.user().data.correctMainAnswer = true;
                                             delete this.user().data.subQuestionsFinished;
                                         } else {
@@ -164,7 +196,9 @@ app.setHandler({
                                         if (this.user().data.hardQuestion !== true) {
                                             this.user().data.mainQuestionIndex++;
                                         }
-                                        this.ask(`${mainQuestion}`);
+                                        this.ask(mainQuestion);
+
+                                        this.user().data.questionToRepeat = mainQuestion;
                                     }
 
                                 });
@@ -196,9 +230,11 @@ app.setHandler({
 
                                         this.user().data.mainQuestionIndex++;
 
-
                                         this.user().data.correctMainAnswer = true;
-                                        this.ask(`${mainQuestion}`);
+                                        this.ask(mainQuestion);
+
+                                        this.user().data.questionToRepeat = mainQuestion;
+
                                     } else {
                                         let valuesToRead = getQuestionAnswers.call(this, prevMainQuestion, prevSubQuestion);
                                         if (valuesToRead !== undefined) {
@@ -214,7 +250,9 @@ app.setHandler({
                                         console.log("this.user().data.hardQuestion !== true")
                                         this.user().data.mainQuestionIndex++;
                                     }
-                                    this.ask(`${mainQuestion}`);
+                                    this.ask(mainQuestion);
+
+                                    this.user().data.questionToRepeat = mainQuestion;
                                 }
 
                             });
@@ -241,7 +279,7 @@ app.setHandler({
 
     },
     'AMAZON.RepeatIntent': function () {
-        this.ask("Repeat");
+        this.ask(this.user().data.questionToRepeat);
     },
     'END': function () {
         console.log("***********STOPPED***************");
@@ -274,12 +312,26 @@ module.exports.app = app;
 
 function getQuestionAnswers(prevMainQuestion, prevSubQuestion) {
     if (survey.surveyQuestions[prevMainQuestion].hasOwnProperty('surveyQuestions') !== true) {
-        let answers = survey.surveyQuestions[prevMainQuestion].questionAnswers;
+        let answerType = survey.surveyQuestions[prevMainQuestion].answerType;
+
         let answersStringToRead = [];
-        for (let m = 0; m < answers.length; m++) {
-            answersStringToRead.push(answers[m].answer);
+        
+        for (let m = 0; m < questionTypes[answerType].length; m++) {
+            answersStringToRead.push(questionTypes[answerType][m].answer);
         }
         if (answersStringToRead.length > 0) {
+            return answersStringToRead;
+        }
+    } else {
+        let answerType = survey.surveyQuestions[prevMainQuestion].surveyQuestions[prevSubQuestion].answerType;
+
+        let answersStringToRead = [];
+
+        for(let m = 0; m < questionTypes[answerType].length; m++) {
+            answersStringToRead.push(questionTypes[answerType][m].answer);
+        }
+
+        if(answersStringToRead.length > 0) {
             return answersStringToRead;
         }
     }
@@ -287,14 +339,12 @@ function getQuestionAnswers(prevMainQuestion, prevSubQuestion) {
 
 
 function compareUserInputAndAnswer(prevMainQuestion, prevSubQuestion, inputs) {
-    // if (prevMainQuestion && prevSubQuestion && slots) {
-    console.log("ARGS :compareUserInputAndAnswer :  ", prevMainQuestion, prevSubQuestion, inputs)
+
     if (survey.surveyQuestions[prevMainQuestion].hasOwnProperty('surveyQuestions') !== true) {
         let answerType = survey.surveyQuestions[prevMainQuestion].answerType;
         console.log("ANSWER_TYPE :", answerType);
         if (answerType !== undefined) {
-            // getQuestionAnswers.call(this, answerType);
-            // console.log("INPUTS: ", inputs);
+      
             for (let n = 0; n < questionTypes[answerType].length; n++) {
                 // console.log("n:", questionTypes[answerType][n]);
                 console.log("comparingValues : ", questionTypes[answerType][n].answer.toLowerCase(), "/", inputs['customAnswer'].key.toLowerCase());
@@ -309,34 +359,33 @@ function compareUserInputAndAnswer(prevMainQuestion, prevSubQuestion, inputs) {
                     break;
                 }
             }
-        } else {
-            console.log("compareUserInputAndAnswer");
         }
     } else {
         let answerType = survey.surveyQuestions[prevMainQuestion].surveyQuestions[prevSubQuestion].answerType;
         console.log("WHERE UNDEFINED: ", answerType);
-        for (let n = 0; n < questionTypes[answerType].length; n++) {
-            // console.log("n:", questionTypes[answerType][n]);
-            console.log("comparingValues : ", questionTypes[answerType][n].answer.toLowerCase(), "/", inputs['customAnswer'].key.toLowerCase());
-            let answerValues = [];
-            if (questionTypes[answerType][n].answer.toLowerCase() === inputs['customAnswer'].key.toLowerCase()) {
-                console.log("inputs['customAnswer'].key.toLowerCase() :", questionTypes[answerType][n].answer);
-                let questionAndAnswerObj = {
-                    question: survey.surveyQuestions[prevMainQuestion].questionId,
-                    answer: questionTypes[answerType][n].answerValue
+        if (answerType !== undefined) { 
+            for (let n = 0; n < questionTypes[answerType].length; n++) {
+                // console.log("n:", questionTypes[answerType][n]);
+                console.log("comparingValues : ", questionTypes[answerType][n].answer.toLowerCase(), "/", inputs['customAnswer'].key.toLowerCase());
+                let answerValues = [];
+                if (questionTypes[answerType][n].answer.toLowerCase() === inputs['customAnswer'].key.toLowerCase()) {
+                    console.log("inputs['customAnswer'].key.toLowerCase() :", questionTypes[answerType][n].answer);
+                    let questionAndAnswerObj = {
+                        question: survey.surveyQuestions[prevMainQuestion].questionId,
+                        answer: questionTypes[answerType][n].answerValue
+                    }
+                    console.log("questionAndAnswerObj: ", questionAndAnswerObj);
+                    return questionAndAnswerObj;
+                    break;
                 }
-                return questionAndAnswerObj;
-                break;
             }
         }
-        // return undefined;
     }
-    // }
 }
 
 function checkUserInput(callback) {
-    // return 'matched';
     let prevMainQuestion, prevSubQuestion;
+
     if (this.user().data.mainQuestionIndex === 0) {
         prevMainQuestion = 0;
     } else {
@@ -348,6 +397,7 @@ function checkUserInput(callback) {
     } else {
         prevSubQuestion = this.user().data.subQuestionIndex - 1;
     }
+    
     let inputs = this.getInputs();
     if (Object.keys(inputs).length > 0) {
         console.log("slots here");
@@ -356,19 +406,4 @@ function checkUserInput(callback) {
         console.log("there is no slots here");
         callback();
     }
-}
-
-let a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
-let b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-function inWords(num) {
-    if ((num = num.toString()).length > 9) return 'overflow';
-    let n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
-    if (!n) return; let str = '';
-    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
-    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
-    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
-    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
-    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + ' ' : '';
-    return str;
 }
