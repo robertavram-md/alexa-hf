6da998234d1ffec5256f6e9f60e6219223ec2d0d
danchul_vitalii <danchul_vitalii@bitbucket.org>
2018-09-19 18:22:31 +0300

    port fix

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const {app} = require('./app/app.js');
 // =================================================================================
 
 if (app.isWebhook()) {
-    const port = process.env.PORT || 3001;
+    const port = process.env.PORT || 3000;
     Webhook.listen(port, () => {
         console.log(`Example server listening on port ${port}!`);
     });
-    Webhook.post('/', (req, res) => {
+    Webhook.post('/webhook', (req, res) => {
         app.handleWebhook(req, res);
     });
 }
